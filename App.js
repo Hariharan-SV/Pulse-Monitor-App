c0ba5088420a02ecd425fd5d76e315067a1dd52c
Hariharan SV <hariharan.i@xpresslane.in>
2022-05-02 21:59:53 +0530

    Initial Setup

--- App.js
+++ App.js
@@ -1,14 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { StyleSheet, Text, Button, View } from 'react-native';
 
-export default function App() {
+
+const Stack = createNativeStackNavigator();
+
+const HomeScreen = ({ navigation }) => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Button
+      title="Login"
+      onPress={() =>
+        navigation.navigate('Profile', { name: 'John Doe' })
+      }
+    />
   );
+};
+const ProfileScreen = ({ navigation, route }) => {
+  const response = {
+    pulse: '80/120',
+    oxygenRate: '90',
+    temperature: '40 C'
+  };
+
+  return (
+    <View>
+      <Text>This is {route.params.name}'s profile</Text>
+      <Text>Pulse: {response.pulse}</Text>
+      <Text>Oxygen Rate: {response.oxygenRate} </Text>
+      <Text>Temperature: {response.temperature}</Text>
+    </View>);
 }
+
+export default App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: 'Pulse Monitor' }}
+        />
+        <Stack.Screen name="Profile" component={ProfileScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
