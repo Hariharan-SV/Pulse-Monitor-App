98d237c27a19d7a07bb3499769d3e5cf125249c0
Hariharan SV <hariharan.i@xpresslane.in>
2022-05-03 21:33:19 +0530

    Add chart for visualization

--- App.js
+++ App.js
@@ -2,6 +2,10 @@ import * as React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StyleSheet, Text, Button, View } from 'react-native';
+import { useEffect, useState } from 'react/cjs/react.development';
+import { LineChart } from 'react-native-chart-kit';
+import { Dimensions } from 'react-native';
+import dayjs from 'dayjs';
 
 
 const Stack = createNativeStackNavigator();
@@ -17,18 +21,59 @@ const HomeScreen = ({ navigation }) => {
   );
 };
 const ProfileScreen = ({ navigation, route }) => {
-  const response = {
-    pulse: '80/120',
-    oxygenRate: '90',
-    temperature: '40 C'
-  };
+  const [response, setResponse] = useState([{
+    "patient_id": 0,
+    "timestamp": "2022-06-01T08:14:24.454000",
+    "type": "temperature",
+    "value": 0,
+  }]);
+
+  useEffect(() => {
+    async function loadScreen() {
+      const jsonResponse = await fetch('http://13.126.59.198/sensor-data/', {
+        method: 'GET',
+      }).then(response => response.json());
+      setResponse(jsonResponse);
+    }
+    loadScreen()
+  }, [])
+
+  const screenWidth = Dimensions.get("window").width;
 
   return (
     <View>
       <Text>This is {route.params.name}'s profile</Text>
-      <Text>Pulse: {response.pulse}</Text>
+      {/* <Text>Pulse: {response.pulse}</Text>
       <Text>Oxygen Rate: {response.oxygenRate} </Text>
-      <Text>Temperature: {response.temperature}</Text>
+      <Text>Temperature: {response.temperature}</Text> */}
+      {/* <Text>{JSON.stringify(response)}</Text> */}
+      <View>
+        <LineChart
+          data={{
+            labels: response.map((t) => dayjs(t.timestamp).format('HH:mm:ss')),
+            datasets: [
+              {
+                data: response.map((t) => t.value),
+                color: (opacity = 1) => `rgba(134, 65, 244, ${opacity})`, // optional
+                strokeWidth: 2 // optional
+              }
+            ],
+            legend: ['Patient Temperature'] // optional
+          }}
+          width={screenWidth}
+          height={220}
+          chartConfig={{
+            backgroundGradientFrom: "#FFF",
+            backgroundGradientFromOpacity: 0,
+            backgroundGradientTo: "#FFF",
+            backgroundGradientToOpacity: 0.5,
+            color: (opacity = 1) => `#000`,
+            strokeWidth: 2, // optional, default 3
+            barPercentage: 0.5,
+            useShadowColorFromDataset: false // optional
+          }}
+        ></LineChart>
+      </View>
     </View>);
 }
 
